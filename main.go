4d01291bbadc1c60998c89e75b40405b89dbe78d
BominRahmani <bominrahmani@gmail.com>
2024-06-30 00:08:52 -0400

    added initial bubbletea layout

--- main.go
+++ main.go
@@ -1,7 +1,96 @@
 package main
 
-import "fmt"
+import (
+	"log"
+
+	"github.com/charmbracelet/bubbles/textinput"
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
+)
+
+type Styles struct {
+	BorderColor lipgloss.Color
+	InputField  lipgloss.Style
+}
+
+func DefaultStyles() *Styles {
+	s := new(Styles)
+	s.BorderColor = lipgloss.Color("36")
+	s.InputField = lipgloss.NewStyle().BorderForeground(s.BorderColor).BorderStyle(lipgloss.NormalBorder()).Padding(1).Width(80)
+	return s
+}
+
+type model struct {
+	movies      []string //change this to more complicated and complete structure later
+	width       int
+	height      int
+	index       int
+	styles      *Styles
+	answerField textinput.Model
+}
+
+func New(movies []string) *model {
+	styles := DefaultStyles()
+	answerField := textinput.New()
+	answerField.Placeholder = ""
+  answerField.Focus()
+	return &model{
+		movies:      movies,
+		answerField: answerField,
+		styles:      styles,
+	}
+}
+
+func (m model) Init() tea.Cmd {
+	return nil
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+  var cmd tea.Cmd
+  switch msg := msg.(type) {
+	case tea.WindowSizeMsg:
+		m.width = msg.Width
+		m.height = msg.Height
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "ctrl+c":
+			return m, tea.Quit
+    case "enter":
+      m.answerField.SetValue("Searching")
+      return m, nil 
+		}
+	}
+  m.answerField, cmd = m.answerField.Update(msg)
+	return m, cmd
+}
+
+func (m model) View() string {
+	if m.width == 0 {
+		return "loading..."
+	}
+	return lipgloss.Place(
+		m.width,
+		m.height,
+		lipgloss.Center,
+		lipgloss.Center,
+		lipgloss.JoinVertical(
+			lipgloss.Center,
+			"What movie would you like to watch?",
+			m.styles.InputField.Render(m.answerField.View()),
+		),
+	)
+}
 
 func main() {
-	fmt.Print("All the world is a stage")
+	movies := []string{"Bullet Train", "Life of Pi", "12 Angry Men", "Inception"}
+	m := New(movies)
+	f, err := tea.LogToFile("debug.log", "debug")
+	if err != nil {
+		log.Fatal("err: %w", err)
+	}
+	defer f.Close()
+	p := tea.NewProgram(m, tea.WithAltScreen())
+	if _, err := p.Run(); err != nil {
+		log.Fatal(err)
+	}
 }
