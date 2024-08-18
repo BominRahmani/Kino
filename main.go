02ecb528cb6887b8825f2c8f9b076df05f06af2a
BominRahmani <bominrahmani@gmail.com>
2024-08-17 23:16:11 -0400

    got rid of bubbletea, use vanilla golang

--- main.go
+++ main.go
@@ -1,231 +1,123 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
-	"log"
+	"hash/fnv"
+	"io"
+	"net/http"
+	"os"
 	"os/exec"
-	"time"
+	"path/filepath"
+	"strings"
+
+	"github.com/pterm/pterm"
+	"github.com/pterm/pterm/putils"
 
 	"github.com/bominrahmani/kino/providers"
-	"github.com/charmbracelet/bubbles/list"
-	"github.com/charmbracelet/bubbles/textinput"
-	tea "github.com/charmbracelet/bubbletea"
-	"github.com/charmbracelet/lipgloss"
 )
-
-type Styles struct {
-	BorderColor lipgloss.Color
-	InputField  lipgloss.Style
-}
-
-func DefaultStyles() *Styles {
-	s := new(Styles)
-	s.BorderColor = lipgloss.Color("36")
-	s.InputField = lipgloss.NewStyle().BorderForeground(s.BorderColor).BorderStyle(lipgloss.NormalBorder()).Padding(1).Width(80)
-	return s
-}
-
-type movieItem struct {
-	movie *providers.Movie
-}
-
-func (i movieItem) Title() string       { return i.movie.Title }
-func (i movieItem) Description() string { return i.movie.Year }
-func (i movieItem) FilterValue() string { return i.movie.Title }
-
-type model struct {
-	movies      []*providers.Movie
-	width       int
-	height      int
-	list        list.Model
-	styles      *Styles
-	answerField textinput.Model
-	state       string
-	loading     bool
-	initialized bool
-	provider    *providers.FlixHQProvider
-	selectedURL string
-}
-
-func New() *model {
-	styles := DefaultStyles()
-	answerField := textinput.New()
-	answerField.Placeholder = "Enter movie title"
-	answerField.Focus()
-
-	// Initialize the list with an empty delegate
-	emptyList := list.New([]list.Item{}, list.NewDefaultDelegate(), 0, 0)
-
-	return &model{
-		answerField: answerField,
-		styles:      styles,
-		state:       "input",
-		list:        emptyList,
-		provider:    providers.NewFlixHQProvider(),
-	}
-}
-
-func (m model) Init() tea.Cmd {
-	return tea.Batch(textinput.Blink, tea.EnterAltScreen)
-}
-
-func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
-	var cmd tea.Cmd
-
-	switch msg := msg.(type) {
-	case tea.WindowSizeMsg:
-		m.width = msg.Width
-		m.height = msg.Height
-		if !m.initialized {
-			m.initialized = true
-			return m, tea.Batch(
-				func() tea.Msg {
-					time.Sleep(100 * time.Millisecond) // Small delay to ensure window size is set
-					return tea.WindowSizeMsg{Width: m.width, Height: m.height}
-				},
-			)
-		}
-
-	case tea.KeyMsg:
-		switch m.state {
-		case "input":
-			switch msg.String() {
-			case "ctrl+c":
-				return m, tea.Quit
-			case "enter":
-				if !m.loading {
-					m.loading = true
-					query := m.answerField.Value()
-					return m, tea.Batch(
-						func() tea.Msg {
-							return searchMsg(query)
-						},
-						tea.Tick(time.Millisecond*100, func(t time.Time) tea.Msg {
-							return tickMsg{}
-						}),
-					)
-				}
-			}
-		case "list":
-			switch msg.String() {
-			case "q", "esc":
-				m.state = "input"
-				m.answerField.SetValue("")
-				return m, nil
-			case "enter":
-				if i, ok := m.list.SelectedItem().(movieItem); ok {
-					m.selectedURL = i.movie.Url
-					kinoFile := m.provider.KinoTime(i.movie.Url)
-					cmd := exec.Command("mpv", kinoFile)
-					err := cmd.Run()
-					if err != nil {
-						fmt.Println("Error running MPV:", err)
-					}
-					return m, tea.Quit
-				}
-			}
-		}
-
-	case searchMsg:
-		return m, func() tea.Msg {
-			start := time.Now()
-			catalogue, err := m.provider.Scrape(string(msg))
-			log.Printf("Scrape took %v", time.Since(start))
-			return searchResultMsg{movies: catalogue, err: err}
-		}
-
-	case searchResultMsg:
-		m.loading = false
-		if msg.err != nil {
-			m.state = "input"
-			m.answerField.SetValue(fmt.Sprintf("Error: %v", msg.err))
-		} else {
-			start := time.Now()
-			m.movies = msg.movies
-			items := make([]list.Item, len(m.movies))
-			for i, kino := range m.movies {
-				items[i] = movieItem{movie: kino}
-			}
-
-			m.list.SetItems(items)
-			m.list.SetSize(m.width, m.height-10)
-			m.list.Title = "Search Results"
-			m.state = "list"
-
-			log.Printf("List initialization took %v", time.Since(start))
-		}
-		return m, nil
-
-	case tickMsg:
-		if m.loading {
-			return m, tea.Tick(time.Millisecond*100, func(t time.Time) tea.Msg {
-				return tickMsg{}
-			})
-		}
-	}
-
-	if m.state == "input" {
-		m.answerField, cmd = m.answerField.Update(msg)
-	} else {
-		m.list, cmd = m.list.Update(msg)
-	}
-
-	return m, cmd
-}
-
-func (m model) View() string {
-	if !m.initialized {
-		return "Initializing..."
-	}
-	var content string
-	switch m.state {
-	case "input":
-		content = lipgloss.JoinVertical(
-			lipgloss.Center,
-			"What movie would you like to watch?",
-			m.styles.InputField.Render(m.answerField.View()),
-		)
-		if m.loading {
-			content = lipgloss.JoinVertical(
-				lipgloss.Center,
-				content,
-				"Searching...",
-			)
-		}
-	case "list":
-		content = m.list.View()
-	}
-	return lipgloss.Place(
-		m.width,
-		m.height,
-		lipgloss.Center,
-		lipgloss.Center,
-		content,
-	)
-}
-
-type searchMsg string
 
 type searchResultMsg struct {
 	movies []*providers.Movie
 	err    error
 }
 
-type tickMsg struct{}
-
 func main() {
-	m := New()
-	f, err := tea.LogToFile("debug.log", "debug")
+	//cacheDir, err := os.Create("./cache/")
+
+	introductionMessage()
+
+	// Prompt user for movie name
+	var movieName string
+	reader := bufio.NewReader(os.Stdin)
+
+	fmt.Print("Enter a movie name: ")
+	movieName, _ = reader.ReadString('\n')
+	movieName = strings.TrimSpace(movieName)
+
+	// Feed movie into engine
+	flixhq := providers.NewFlixHQProvider()
+	catalogue, err := flixhq.Scrape(movieName)
 	if err != nil {
-		log.Fatal("err: %w", err)
+		fmt.Println(err)
+		return
 	}
-	defer f.Close()
-	p := tea.NewProgram(m, tea.WithAltScreen())
-	if m, err := p.Run(); err != nil {
-		log.Fatal(err)
-	} else {
-		if finalModel, ok := m.(model); ok && finalModel.selectedURL != "" {
-			fmt.Printf("Selected movie URL: %s\n", finalModel.selectedURL)
+
+	// Download and cache all the images within a temporary directory
+	downloadImages(catalogue)
+
+	// Use FZF to select a movie
+	selectedMovie, err := FZFSearch(catalogue)
+	if err != nil {
+		fmt.Println("Error selecting movie:", err)
+		return
+	}
+
+  fmt.Println(selectedMovie)
+
+}
+
+func hash(s string) uint32 {
+        h := fnv.New32a()
+        h.Write([]byte(s))
+        return h.Sum32()
+}
+
+func downloadImages(catalogue []*providers.Movie) {
+	for _, movie := range catalogue {
+		resp, err := http.Get(movie.ImageUrl)
+		if err != nil {
+			fmt.Println("Error downloading image previews: ", err)
+			return
+		}
+
+		defer resp.Body.Close()
+
+    imageHash := hash(movie.ImageUrl) 
+    imageFile := filepath.Join("/tmp/", fmt.Sprint(imageHash)) 
+		out, err := os.Create(imageFile)
+
+		if err != nil {
+      fmt.Println("Error downloading image previews: ", err)
+			return
+		}
+		defer out.Close()
+
+		_, err = io.Copy(out, resp.Body)
+		if err != nil {
+			fmt.Println("Error downloading image previews: ", err)
+			return
 		}
 	}
+}
+
+func FZFSearch(catalogue []*providers.Movie) (*providers.Movie, error) {
+	var input strings.Builder
+	for _, movie := range catalogue {
+		input.WriteString(fmt.Sprintf("%s (%s)\n", movie.Title, movie.Year))
+	}
+
+	cmd := exec.Command("fzf", "--ansi")
+	cmd.Stdin = strings.NewReader(input.String())
+	cmd.Stderr = os.Stderr
+
+	output, err := cmd.Output()
+	if err != nil {
+		return nil, err
+	}
+
+	selectedTitle := strings.TrimSpace(string(output))
+	for _, movie := range catalogue {
+		if fmt.Sprintf("%s (%s)", movie.Title, movie.Year) == selectedTitle {
+			return movie, nil
+		}
+	}
+
+	return nil, fmt.Errorf("selected movie not found in catalogue")
+}
+
+func introductionMessage() {
+	s, _ := pterm.DefaultBigText.WithLetters(putils.LettersFromString("KINO")).Srender()
+	pterm.DefaultCenter.Println(s)
+	pterm.DefaultCenter.WithCenterEachLineSeparately().Println("Totus mundus agit histrionem")
 }
---
+++ todo.md
@@ -0,0 +1,16 @@
+# TODO
+- Implement standardbox that pops up cool message at the top when you start application [x]
+
+- Implement FZF as the engine for prompting/searches []
+  - Navigation []
+
+- Implement sixel/chafa as image output []
+
+- Implement image caching for movies []
+
+
+
+## Useful tools
+- cfmt allows text to be colorized
+- go-palette paints terminal 
+
